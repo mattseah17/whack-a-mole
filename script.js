4eca49849adc802a144f3545424cbec15e164425
mattseah17 <matthewseah17@gmail.com>
2022-06-08 10:47:51 +0800

    game completed (with stretch goals)

--- script.js
+++ script.js
@@ -8,42 +8,6 @@ const sound2 = new Audio("sounds/mixkit-retro-arcade-game-over-470.mp3");
 
 let score = 0;
 
-function runGame() {
-  const num = Math.ceil(Math.random() * 90);
-  const i = Math.floor(Math.random() * holes.length);
-  const hole = holes[i];
-  let timer = null;
-  
-  const moleDiv = document.createElement("div");
-  moleDiv.classList.add("mole");
-  const moleText = document.createElement("div");
-  moleText.setAttribute("id", "moletext");
-  moleDiv.append(moleText);
-  const img = document.createElement("img");
-  moleDiv.append(img);
-  moleText.innerText = num;
-  
-  moleDiv.addEventListener("click", () => {
-    moleDiv.style.background = "none";
-    img.src = "images/diglett-whacked.png";
-    score += 2;
-    sound1.play();
-    scoreEl.textContent = score;
-    clearTimeout(timer);
-    setTimeout(() => {
-      hole.removeChild(moleDiv);
-      runGame();
-    }, 500);
-  });
-
-  hole.appendChild(moleDiv);
-
-  timer = setTimeout(() => {
-    hole.removeChild(moleDiv);
-    runGame();
-  }, 20000);
-}
-
 //Hammer cursor movement
 document.querySelector(".board").addEventListener("mousemove", (e) => {
   cursor.style.top = e.pageY + "px";
@@ -55,11 +19,66 @@ document.querySelector(".board").addEventListener("mousedown", () => {
 document.querySelector(".board").addEventListener("mouseup", () => {
   cursor.classList.remove("active");
 });
-//Start button click
-document.querySelector(".startTimer").addEventListener("click", () => {
-  document.querySelector("button").remove();
-  countdown();
-  runGame();
+
+//Dropdown selection, start game
+const select = document.getElementById("multiples");
+select.addEventListener("change", (e) => {
+  const val = e.target.value;
+  document.querySelector(".startTimer").addEventListener("click", () => {
+    document.querySelector("button").remove();
+    countdown();
+
+    function runGame() {
+      const num = Math.ceil(Math.random() * 50);
+      const i = Math.floor(Math.random() * holes.length);
+      const hole = holes[i];
+      let timer = null;
+
+      const moleDiv = document.createElement("div");
+      moleDiv.classList.add("mole");
+      const moleText = document.createElement("div");
+      moleText.setAttribute("id", "moletext");
+      moleDiv.append(moleText);
+      const img = document.createElement("img");
+      moleDiv.append(img);
+      moleText.innerText = num;
+      moleDiv.setAttribute("value", num);
+
+      moleDiv.addEventListener("click", (e) => {
+        if (e.target.getAttribute("value") % val === 0) {
+          moleDiv.style.background = "none";
+          img.src = "images/diglett-whacked.png";
+          score += 2;
+          sound1.play();
+          scoreEl.textContent = score;
+          clearTimeout(timer);
+          setTimeout(() => {
+            hole.removeChild(moleDiv);
+            runGame();
+          }, 500);
+        } else {
+          moleDiv.style.background = "none";
+          img.src = "images/diglett_shy.png";
+          score -= 1;
+          scoreEl.textContent = score;
+          clearTimeout(timer);
+          setTimeout(() => {
+            hole.removeChild(moleDiv);
+            runGame();
+          }, 500);
+        }
+      });
+
+      hole.appendChild(moleDiv);
+
+      timer = setTimeout(() => {
+        hole.removeChild(moleDiv);
+        runGame();
+      }, 1500);
+    }
+
+    runGame(val);
+  });
 });
 
 //Game timer
@@ -87,7 +106,3 @@ function countdown() {
     }
   }
 }
-
-
-
-
